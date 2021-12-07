c9b0e97c31b0ee8e355de23c310f6e588cec8a54
ATLAS2021 <78792188+ATLAS2021@users.noreply.github.com>
2021-12-07 18:18:16 +0100

    horaire not exsit a traiter

--- src/Service/HoraireService.php
+++ src/Service/HoraireService.php
@@ -33,12 +33,21 @@ class HoraireService
     {
         $this->em = $em;
     }
-
+    private $dateTime;
     public function getHoraireForDate(DateTime $dateTime): ?Horaire
     {
-        $nowDateTime = new DateTime();
+        $this->dateTime= $dateTime;
         $this->horaire = current(array_filter(array_map(
-            fn ($horaire): ?Horaire => $horaire->getDateFin() ? (($horaire->getDateDebut() <= $dateTime and $dateTime  <= $horaire->getDateFin()) ? $horaire : ($horaire->getDateDebut() <= $dateTime and $dateTime  <= $nowDateTime) ? $horaire : null)            )),
+            function (Horaire $horaire): ?Horaire {
+                if ($horaire->getDateFin()) {
+                    return ($horaire->getDateDebut() <= $this->dateTime and $this->dateTime  <= $horaire->getDateFin()) ? $horaire : null ;
+                } else {
+                    $nowDateTime = new DateTime();
+                    dump($horaire->getDateDebut(), $horaire->getDateFin(), $this->dateTime, $nowDateTime);
+                    return ($horaire->getDateDebut() <= $this->dateTime and $nowDateTime  <= $horaire->getDateFin()) ? $horaire : null ;
+                }
+                //  ($horaire->getDateDebut() <= $dateTime and $dateTime  <= $horaire->getDateFin()) ? $horaire : null : ($horaire->getDateDebut() <= $dateTime and $dateTime  <= $nowDateTime) ? $horaire : null,
+            },
             $this->em->getRepository(Horaire::class)->findAll()
         )));
         if ($this->horaire) {
